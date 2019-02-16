404d71270ab89bb1d994808fbc2025dc2374f325
andmorefine <andmorefine@gmail.com>
2019-02-17 03:26:39 +0900

    fetch

--- fetchWebPage.py
+++ fetchWebPage.py
@@ -1,70 +1,68 @@
+
+import json
+import csv
+from pprint import pprint
+
+# 連番や等差数列を生成する関数
+import numpy as np
 # coding: UTF-8
 import urllib.request as ur
+import urllib.parse
 from bs4 import BeautifulSoup
 
-# アクセスするURL
-url = "http://www.seiku.net/kotowaza/99_01.html"
+headers = { 'Content-Type': 'application/json', }
 
-# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
-html = ur.urlopen(url)
+# csv 開く
+file = open('sample.csv', 'w')
+writer = csv.writer(file, lineterminator='\n')
 
-# htmlをBeautifulSoupで扱う
-soup = BeautifulSoup(html, "html.parser")
+# url_link = np.array(range(1, 45))
+url_link = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44']
 
-# # タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
-# title_tag = soup.title
+link_page = np.array(range(2, 10))
+link_page = ['', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9']
 
-# # 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
-# title = title_tag.string
+for link in url_link:
+  for page in link_page:
+    # アクセスするURL
+    url = "http://www.seiku.net/kotowaza/99_%s%s.html" % (link, page)
 
-# # タイトル要素を出力
-# print(title_tag)
+    # URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
 
-# # タイトルを文字列を出力
-# print(title)
+    try:
+      html = ur.urlopen(url)
+    except:
+      print('pass')
+      continue
+      pass
 
-# span要素全てを摘出する→全てのspan要素が配列に入ってかえされます→[<span class="m-wficon triDown"></span>, <span class="l-h...
-dl = soup.find_all("dl")
+    # htmlをBeautifulSoupで扱う
+    soup = BeautifulSoup(html, "html.parser")
 
-# print時のエラーとならないように最初に宣言しておきます。
-# nikkei_heikin = ""
-kotowaza_all = []
-# for分で全てのspan要素の中からClass="mkc-stock_prices"となっている物を探します
-for i, tag in enumerate(dl):
-    kotowaza = []
-    title = []
-    # classの設定がされていない要素は、tag.get("class").pop(0)を行うことのできないでエラーとなるため、tryでエラーを回避する
-    dd = tag.find_all('dd')
-    # print(tag.dt.text)
-    title = tag.dt.text.strip().split("\n")
-    print(title[0])
+    # span要素全てを摘出する→全てのspan要素が配列に入ってかえされます→[<span class="m-wficon triDown"></span>, <span class="l-h...
+    dl = soup.find_all("dl")
 
-    kotowaza.append(i)
-    kotowaza.append(title[0])
-    kotowaza.append(title[1])
-    kotowaza.append(dd[1].string)
+    # print時のエラーとならないように最初に宣言しておきます。
+    kotowaza_all = []
+    # for分で全てのspan要素の中からClass="mkc-stock_prices"となっている物を探します
+    for i, tag in enumerate(dl):
+      kotowaza = []
+      title = []
+      # classの設定がされていない要素は、tag.get("class").pop(0)を行うことのできないでエラーとなるため、tryでエラーを回避する
+      dd = tag.find_all('dd')
+      # print(tag.dt.text)
+      title = tag.dt.text.strip().split("\n")
 
-    kotowaza_all.append(kotowaza)
-    # try:
-        # tagの中からclass="n"のnの文字列を摘出します。複数classが設定されている場合があるので
-        # get関数では配列で帰ってくる。そのため配列の関数pop(0)により、配列の一番最初を摘出する
-        # <span class="hoge" class="foo">  →   ["hoge","foo"]  →   hoge
-        # string_ = tag.get("class").pop(0)
+      kotowaza.append(i)
+      kotowaza.append(title[0])
+      kotowaza.append(title[1])
+      kotowaza.append(dd[1].string)
 
-        # print(tag)
+      kotowaza_all.append(kotowaza)
 
-        # # 摘出したclassの文字列にmkc-stock_pricesと設定されているかを調べます
-        # if string_ in "news":
-        #     # mkc-stock_pricesが設定されているのでtagで囲まれた文字列を.stringであぶり出します
-        #     nikkei_heikin = tag.string
-        #     # print(tag.string)
-        #     # 摘出が完了したのでfor分を抜けます
-        #     break
-    # except:
-    #     # パス→何も処理を行わない
-    #     pass
+    # writer.writerow(list)
+    # 二次元配列
+    writer.writerows(kotowaza_all)
 
-# 摘出した日経平均株価を出力します。
-# print(nikkei_heikin)
-
-print(kotowaza_all)
+# CSVファイルを閉じる
+file.close()
